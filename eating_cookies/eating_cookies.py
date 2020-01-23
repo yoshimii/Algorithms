2f7013219565e287c2e3368d7b1fd197fc804c9d
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-01-23 01:21:00 -0600

    attempt at caching

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,6 +6,7 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
+  cache = { }
   if n < 0:
     return 0
   elif n == 0:
@@ -13,6 +14,19 @@ def eating_cookies(n, cache=None):
   else:
     return eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
 
+# def dynamic_cookies(n, cache=None):
+#   if cache is None:
+#     cache = {0:1}
+
+#   if n < 0:
+#     print(0)
+#     return 0
+#   elif n not in cache:
+#     cache[n] = dynamic_cookies(n - 3) + dynamic_cookies(n - 2) + dynamic_cookies(n - 1)
+#   print(cache[n])  
+#   return cache[n]
+# dynamic_cookies(50)
+  
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_cookies = int(sys.argv[1])
